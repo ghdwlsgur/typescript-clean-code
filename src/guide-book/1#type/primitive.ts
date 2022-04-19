1410b66b0697842d326b9e4089e9baf55e148bc4
HongJinHyeok <redmax45@naver.com>
2022-04-19 12:50:38 +0900

    🐛 eslint

--- src/guide-book/1#type/primitive.ts
+++ src/guide-book/1#type/primitive.ts
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 /**
  * 원시 데이터 타입 number, string, boolean을 명시적으로 설정한 변수 선언은 다음과 같이 작성합니다.
  */
 // 명시적으로 number 타입을 설정
-let product_id: number = 124981;
+const product_id: number = 124981;
 
 // 명시적으로 string 타입을 설정
-let product_name: string = 'VR 글래스';
+const product_name: string = 'VR 글래스';
 
 // 명시적으로 boolean 타입을 설정
-let is_waterprofing: boolean = false;
+const is_waterprofing: boolean = false;
 
 /**
  * 설정된 데이터 타입이 아닌 데이터 타입을 할당하려 할 경우 TypeScript는 컴파일 과정에서
