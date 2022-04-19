e0ff2680643a3f111103b080c559ecd0dd0c55fa
HongJinHyeok <redmax45@naver.com>
2022-04-19 13:17:38 +0900

    🐛  prefer-const option off

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 'latest',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -21,5 +22,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/prefer-const': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'prefer-const': 'off',
   },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  rootDir: "src",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: 'src',
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
-  coverageDirectory: "coverage",
-  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "!src/**/*.d.ts"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
